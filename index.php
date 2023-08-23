4e9301850250e1b139c1c42716ab1968253a77ab
RafiAtha28 <rafiathasyauqi28@gmail.com>
2023-08-23 11:19:29 +0700

    first-commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<div class="grid-container">
+  <div class="grid-item kuning">Kuning</div>
+  <div class="grid-item hitam c-white">Hitam</div>
+  <div class="grid-item hijau">Hijau</div>
+  <div class="grid-item coklat c-white">Coklat</div>
+  <div class="grid-item merah">Merah</div>
+  <div class="grid-item putih">Putih</div>
+  <div class="grid-item pink">Pink</div>
+  <div class="grid-item biru">Biru</div>
+  <div class="grid-item biru-tua">Biru Tua</div>
+  <div class="grid-item abu">Abu</div>
+  <div class="grid-item orange">Orange</div>
+  <div class="grid-item ungu c-white">Ungu</div>
+</div>
+</body>
+</html>
